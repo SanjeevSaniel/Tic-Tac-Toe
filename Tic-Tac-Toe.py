00868a6998df0322f18a0d7c81d2d3d689604da3
Sanjeev Saniel Kujur <sanjeevsanielkujur7@outlook.com>
2020-06-30 10:49:15 +0530

    Update Comments

--- Tic-Tac-Toe.py
+++ Tic-Tac-Toe.py
@@ -1,5 +1,6 @@
 ''' We will make the board using dictionary in which keys will be the location
-    (i.e. : top-left, mid-right, etc.) and initially it's values will be empty space and then after every move we will change the value according to player's choice of move. '''
+    (i.e. : top-left, mid-right, etc.) and initially it's values will be empty space 
+    and then after every move we will change the value according to player's choice of move. '''
 
 theBoard = {'7': ' ', '8': ' ', '9': ' ',
             '4': ' ', '5': ' ', '6': ' ',
@@ -10,8 +11,9 @@ board_keys = []
 for key in theBoard:
     board_keys.append(key)
 
-''' We will have to print the updated board after every move in the game and thus we will make a function in which we'll define the printBoard function so that we can easily print the board everytime by calling this function. '''
-
+''' We will have to print the updated board after every move in the game and thus we will make a function 
+    in which we'll define the printBoard function so that we can easily print the board 
+    everytime by calling this function. '''
 
 def printBoard(board):
     print(board['7'] + ' | ' + board['8'] + '|' + board['9'])
@@ -21,7 +23,6 @@ def printBoard(board):
     print(board['1'] + ' | ' + board['2'] + '|' + board['3'])
 
 # Now we'll write the main function which has all the gameplay functionality.
-
 
 def game():
     turn = 'X'
@@ -36,7 +37,6 @@ def game():
         if theBoard[move] == ' ':
             theBoard[move] = turn
             count += 1
-
         else:
             print("That place is already filled. \n Move to which place?")
             continue
